93157e00b3bce5a177a616451a0356ecf2dc2f1e
Weronika Kołtonowska <wkoltonowska98@gmail.com>
2025-06-27 00:17:42 +0200

    Fix corse #3

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const app = express();
 
 app.use(cors({
   origin: 'http://localhost:8080', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type']
 }));
 
